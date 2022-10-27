294838ce43b4a3ef70fa16a49ec78ef51dc5fca2
MaryUp <maryup.for.reg@gmail.com>
2022-10-27 09:01:42 +0300

    TaskSix

--- src/com/tinkoffTasks/TaskSixth.java
+++ src/com/tinkoffTasks/TaskSixth.java
@@ -10,38 +10,46 @@ public class TaskSixth {
         for (int i = 0; i < n; i++) {
             players[i] = scanner.nextInt();
         }
-        System.out.println(findIndexToChange(players, 0) + " " + findIndexToChange(players, 1));
-
-    }
-    private static int findIndexToChange(int[] players, int n) {
-        if (isReal(players)) {
-            for (int i = n; i < players.length; i += 2) {
-                if (n == 0 && players[i] % 2 == 0) {
-                    return ++i; //номер игрока больше чем индекс в массиве на 1
-                }
-                if (n == 1 && players[i] % 2 != 0) {
-                    return ++i;
-                }
-            }
-        }
-        return -1;
+        foundIndexes(players);
     }
 
-    private static boolean isReal(int[] players) {
-        // проверяем сколько перестановок нам понадобится
-        int countEven = 0;
-        int countUneven = 0;
-        for (int i = 1; i < players.length; i += 2) {
-            if (players[i - 1] % 2 == 0) {
-                countEven++;
-            }
-            if (players[i] % 2 != 0) {
-                countUneven++;
+    private static int countWrongPositions = 0;
+    private static int firstIndex = -1;
+    private static int secondIndex = -1;
+
+    private static void foundCountWrongPositions(int[] players) {
+        for (int i = 0; i < players.length; i++) {
+            if ((players[i] + i) % 2 == 0) {
+                countWrongPositions++;
+                if (firstIndex == -1) {
+                    firstIndex = i;
+                } else if (secondIndex == -1) {
+                    secondIndex = i;
+                }
             }
         }
-        if (countEven == countUneven && countEven == 1) {
-            return true;
+        System.out.println(countWrongPositions);
+    }
+
+    private static void foundIndexes(int[] players) {
+        foundCountWrongPositions(players);
+        if (countWrongPositions != 2) {
+            firstIndex = -1;
+            secondIndex = -1;
+        } else if (players[firstIndex] % 2 == 0 && players[secondIndex] % 2 == 1){
+            firstIndex++;
+            secondIndex++;
+        } else if (players[firstIndex] % 2 == 1 && players[secondIndex] % 2 == 0) {
+            firstIndex++;
+            secondIndex++;
+        } else {
+            firstIndex = -1;
+            secondIndex = -1;
         }
-        return false;
+
+        System.out.println(firstIndex + " " + secondIndex);
+
     }
 }
+
+
