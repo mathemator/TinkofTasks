4533b41054144fc11d7c81b9643278218c1651a9
MaryUp <maryup.for.reg@gmail.com>
2022-10-27 12:51:48 +0300

    TaskSix

--- src/com/tinkoffTasks/TaskSixth.java
+++ src/com/tinkoffTasks/TaskSixth.java
@@ -10,46 +10,30 @@ public class TaskSixth {
         for (int i = 0; i < n; i++) {
             players[i] = scanner.nextInt();
         }
-        foundIndexes(players);
+        for (int index : findIndexes(players)) {
+            System.out.println(index);
+        }
     }
-
-    private static int countWrongPositions = 0;
-    private static int firstIndex = -1;
-    private static int secondIndex = -1;
-
-    private static void foundCountWrongPositions(int[] players) {
+    private static int[] findIndexes(int[] players) {
+        int[] pair = {-1, -1};
+        int firstIndex = 0;
+        int secondIndex = 0;
+        int countWrongPositions = 0;
         for (int i = 0; i < players.length; i++) {
             if ((players[i] + i) % 2 == 0) {
                 countWrongPositions++;
-                if (firstIndex == -1) {
+                if (firstIndex == 0) {
                     firstIndex = i;
-                } else if (secondIndex == -1) {
+                } else if (secondIndex == 0) {
                     secondIndex = i;
                 }
             }
         }
-       // System.out.println(countWrongPositions);
-    }
-
-    private static void foundIndexes(int[] players) {
-        foundCountWrongPositions(players);
-        if (countWrongPositions == 2) {
-            if (players[firstIndex] % 2 == 0 && players[secondIndex] % 2 == 1) {
-                firstIndex++;
-                secondIndex++;
-            } else if (players[firstIndex] % 2 == 1 && players[secondIndex] % 2 == 0) {
-                firstIndex++;
-                secondIndex++;
-            } else {
-                firstIndex = -1;
-                secondIndex = -1;
-            }
-        } else {
-            firstIndex = -1;
-            secondIndex = -1;
+        if (countWrongPositions == 2 && (players[firstIndex] + players[secondIndex]) % 2 != 0) {
+            pair[0] = firstIndex + 1;
+            pair[1] = secondIndex + 1;
         }
-
-        System.out.println(firstIndex + " " + secondIndex);
+        return pair;
     }
 }
 
