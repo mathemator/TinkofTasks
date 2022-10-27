67573cffbb4d078f2569761743c8c4420058e1c9
MaryUp <maryup.for.reg@gmail.com>
2022-10-27 12:29:53 +0300

    TaskSix

--- src/com/tinkoffTasks/TaskSixth.java
+++ src/com/tinkoffTasks/TaskSixth.java
@@ -28,27 +28,28 @@ public class TaskSixth {
                 }
             }
         }
-        System.out.println(countWrongPositions);
+       // System.out.println(countWrongPositions);
     }
 
     private static void foundIndexes(int[] players) {
         foundCountWrongPositions(players);
-        if (countWrongPositions != 2) {
-            firstIndex = -1;
-            secondIndex = -1;
-        } else if (players[firstIndex] % 2 == 0 && players[secondIndex] % 2 == 1){
-            firstIndex++;
-            secondIndex++;
-        } else if (players[firstIndex] % 2 == 1 && players[secondIndex] % 2 == 0) {
-            firstIndex++;
-            secondIndex++;
+        if (countWrongPositions == 2) {
+            if (players[firstIndex] % 2 == 0 && players[secondIndex] % 2 == 1) {
+                firstIndex++;
+                secondIndex++;
+            } else if (players[firstIndex] % 2 == 1 && players[secondIndex] % 2 == 0) {
+                firstIndex++;
+                secondIndex++;
+            } else {
+                firstIndex = -1;
+                secondIndex = -1;
+            }
         } else {
             firstIndex = -1;
             secondIndex = -1;
         }
 
         System.out.println(firstIndex + " " + secondIndex);
-
     }
 }
 
