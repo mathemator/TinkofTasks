cfbf8ce66f693f63345ee7e83b3f08f9d656fcf9
MaryUp <maryup.for.reg@gmail.com>
2022-11-08 00:30:12 +0300

    task6

--- src/com/example/tinkoff/TaskSixth.java
+++ src/com/example/tinkoff/TaskSixth.java
@@ -1,43 +1,90 @@
 package com.example.tinkoff;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class TaskSixth {
-    //правильное решение
+    static class Player {
+        private int number;
+        private int position;
+        private boolean isRightPos;
+
+        Player(int position, int number) {
+            this.number = number;
+            this.position = position;
+            if ((number + position) % 2 == 0) {
+                isRightPos = true;
+            } else {
+                isRightPos = false;
+            }
+        }
+
+        public boolean isRightPos() {
+            return isRightPos;
+        }
+
+        public int getPosition() {
+            return position;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int[] players = new int[n];
+        int[] numbers = new int[n];
         for (int i = 0; i < n; i++) {
-            players[i] = scanner.nextInt();
+            numbers[i] = scanner.nextInt();
         }
-        int[] pair = findIndexes(players);
-        System.out.println(pair[0] + " " + pair[1]);
+        int[] indexes = findIndexes(numbers);
+        System.out.println(indexes[0] + " " + indexes[1]);
+
     }
 
-    private static boolean checkPlayers(int[] players) {
-        for (int i = 0; i < players.length; i++) {
-            if ((i + players[i]) % 2 == 0) {
+    protected static int[] findIndexes(int[] numbers) {
+        Player[] players = fillPlayers(numbers);
+        if (checkPositions(players)) {
+            return new int[]{1, 3};
+        }
+        List<Player> wrongEvenPos = new ArrayList();
+        List<Player> wrongNotEvenPos = new ArrayList();
+        for (Player player : players) {
+            if (!player.isRightPos()) {
+                if (player.getPosition() % 2 == 0) {
+                    wrongEvenPos.add(player);
+                } else {
+                    wrongNotEvenPos.add(player);
+                }
+            }
+        }
+        if (wrongEvenPos.size() == 1 && wrongNotEvenPos.size() == 1) {
+            int firstIndex = wrongEvenPos.get(0).getPosition();
+            int secondIndex = wrongNotEvenPos.get(0).getPosition();
+            if (firstIndex < secondIndex) {
+                return new int[]{firstIndex, secondIndex};
+            }
+            return new int[]{secondIndex, firstIndex};
+        }
+        return new int[]{-1, -1};
+    }
+
+    private static Player[] fillPlayers(int[] numbers) {
+        int n = numbers.length;
+        Player[] players = new Player[n];
+        for (int i = 0; i < n; i++) {
+            players[i] = new TaskSixth.Player(i + 1, numbers[i]);
+
+        }
+        return players;
+    }
+
+    private static boolean checkPositions(Player[] players) {
+        for (Player player : players) {
+            if (!player.isRightPos) {
                 return false;
             }
         }
         return true;
     }
-
-    private static int[] findIndexes(int[] players) {
-        int[] pair = new int[]{-1, -1};
-        for (int i = 0; i < players.length; i++) {
-            for (int j = i + 1; j < players.length; j++) {
-                int[] copyPlayers = Arrays.copyOf(players, players.length);
-                int tmp = copyPlayers[i];
-                copyPlayers[i] = copyPlayers[j];
-                copyPlayers[j] = tmp;
-                if (checkPlayers(copyPlayers)) {
-                    return new int[]{i + 1, j + 1};
-                }
-            }
-        }
-        return pair;
-    }
 }
+
---
+++ src/com/example/tinkoff/TaskSixth1.java
@@ -0,0 +1,42 @@
+package com.example.tinkoff;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TaskSixth1 {
+    //правильное решение
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] players = new int[n];
+        for (int i = 0; i < n; i++) {
+            players[i] = scanner.nextInt();
+        }
+        int[] pair = findIndexes(players);
+        System.out.println(pair[0] + " " + pair[1]);
+    }
+
+    private static boolean checkPlayers(int[] players) {
+        for (int i = 0; i < players.length; i++) {
+            if ((i + players[i]) % 2 == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static int[] findIndexes(int[] players) {
+        for (int i = 0; i < players.length; i++) {
+            for (int j = i + 1; j < players.length; j++) {
+                int[] copyPlayers = Arrays.copyOf(players, players.length);
+                int tmp = copyPlayers[i];
+                copyPlayers[i] = copyPlayers[j];
+                copyPlayers[j] = tmp;
+                if (checkPlayers(copyPlayers)) {
+                    return new int[]{i + 1, j + 1};
+                }
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
