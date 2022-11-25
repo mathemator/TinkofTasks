618ed3441dd29e9752389993fdb89237c80e7c4e
MaryUp <maryup.for.reg@gmail.com>
2022-11-25 19:16:39 +0300

    task9

--- src/com/example/tinkoff/TaskNine.java
+++ src/com/example/tinkoff/TaskNine.java
@@ -3,65 +3,136 @@ package com.example.tinkoff;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 
 public class TaskNine {
-
     public static void main(String[] args) throws IOException {
-        List<Integer> lunches = new ArrayList<>();
+
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int count = Integer.parseInt(bufferedReader.readLine());
+        int[] prices = new int[count];
         for (int i = 0; i < count; i++) {
-            lunches.add(Integer.parseInt(bufferedReader.readLine()));
+            prices[i] = Integer.parseInt(bufferedReader.readLine());
         }
-        Lunches lunch = new Lunches(lunches);
-        lunch.sumWithSale();
-        System.out.println(lunch.getSum());
+        Lunches lunches = new Lunches(prices);
+        System.out.println(lunches.sum());
+    }
+}
+
+class Lunches {
+    private List<Lunch> lunches = new ArrayList<>();
+
+    public Lunches(int[] prices) {
+        for (int i = 0; i < prices.length; i++) {
+            lunches.add(new Lunch(i, prices[i]));
+        }
+    }
+
+    private Map<Lunch, Lunch> economy = new HashMap<>();
+
+    public int sum() {
+        Lunch firstCoupon = getNextCoupon(lunches.size());
+        if (firstCoupon != null) {
+            economy(firstCoupon);
+        }
+        System.out.println("_________________________________");
+        for (Map.Entry<Lunch, Lunch> pair : economy.entrySet()) {
+            System.out.println("coupon: " + pair.getKey() + " bue: " + pair.getValue());
+        }
+        int sum = 0;
+        for (Lunch lunch : lunches) {
+            if (economy.values().contains(lunch)) {
+                continue;
+            }
+            sum += lunch.getPrice();
+        }
+
+        return sum;
+    }
+
+    private void economy(Lunch lunch) {
+        Lunch maxPrice = getMaxPrice(lunch);
+        if (economy.containsKey(maxPrice)) {
+            Lunch maxPrice1 = getExpectedMaxPrice(lunch);
+            if (maxPrice1.getPrice() == 0) {
+                Lunch nextCoupon = getNextCoupon(lunch.getNumber());
+                if (nextCoupon != null) {
+                    economy(nextCoupon);
+                }
+            }
+            Lunch maxPrice2 = economy.get(maxPrice);
+            if (maxPrice1.getPrice() + maxPrice2.getPrice() > maxPrice.getPrice()) {
+                economy.put(lunch, maxPrice1);
+            } else {
+                economy.remove(maxPrice);
+                economy.put(lunch, maxPrice);
+            }
+        } else {
+            economy.put(lunch, maxPrice);
+        }
+        Lunch nextCoupon = getNextCoupon(lunch.getNumber());
+        if (nextCoupon != null) {
+            economy(nextCoupon);
+        }
+
 
     }
-     public static class Lunches {
 
-        private  List<Integer> lunches = new ArrayList<>();
-        private  int sum = 0;
-        private  int maxPrice = -1;
 
-        public Lunches(List<Integer> lunches) {
-            this.lunches = lunches;
-        }
-
-        public int getSum() {
-            return sum;
-        }
-
-        public void sumWithSale() {
-            int price = 0;
-            do {
-                price = lunches.get(0);
-                if (price != maxPrice) {
-                    sum += price;
-                }
-                lunches.remove(0);
-            } while (price <= 100 && lunches.size() > 0);
-            if (lunches.size() > 0) {
-                maxPrice = maxPrice();
-                sumWithSale();
+    private Lunch getMaxPrice(Lunch lunch) {
+        Lunch maxPrice = new Lunch(0, 0);
+        for (int i = lunch.getNumber() + 1; i < lunches.size(); i++) {
+            Lunch newLunch = lunches.get(i);
+            if (!economy.values().contains(newLunch) && newLunch.getPrice() > maxPrice.getPrice()) {
+                maxPrice = newLunch;
             }
         }
+        return maxPrice;
+    }
 
-         private int maxPrice() {
-            int maxPrice = -1;
-            if (lunches.size() > 0) {
-                for (Integer price : lunches) {
-                    if (price > 100) {
-                        return maxPrice;
-                    } else if (price > maxPrice) {
-                        maxPrice = price;
-                    }
-                }
+    private Lunch getExpectedMaxPrice(Lunch lunch) {
+        Lunch maxPrice = new Lunch(0, 0);
+        for (int i = lunch.getNumber() + 1; i < lunches.size(); i++) {
+            Lunch newLunch = lunches.get(i);
+            if (newLunch.getPrice() > maxPrice.getPrice() && !economy.values().contains(newLunch) && !economy.containsKey(newLunch)) {
+                maxPrice = newLunch;
             }
-            return maxPrice;
         }
+        return maxPrice;
+    }
+
+
+    private Lunch getNextCoupon(int index) {
+        for (int i = index - 1; i >= 0; i--) {
+            Lunch lunch = lunches.get(i);
+            if (lunch.getPrice() > 100 && lunch.getNumber() != lunches.size() - 1) {
+                return lunch;
+            }
+        }
+        return null;
+    }
+}
+
+class Lunch {
+    private int number;
+    private int price;
+
+    public Lunch(int number, int price) {
+        this.number = number;
+        this.price = price;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return number + " : " + price;
     }
 }
---
+++ test/com/example/tinkoff/TaskNineTest.java
@@ -0,0 +1,25 @@
+package com.example.tinkoff;
+
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TaskNineTest extends TestCase {
+
+    @Test
+    public void test1() {
+      //  test(235, new int[]{35, 40, 101, 59, 63});
+        test(621, new int[]{20,50,101,200,150,50,150,20,300,10,50,101,20});
+        test(621, new int[]{20,101,50,10,300,20,150,50,150,200,101,50,20});
+        test(303, new int[]{101, 101, 101, 101, 101});
+        test(303, new int[]{101, 101, 101, 101, 101, 101});
+        test(302, new int[]{20, 101, 200, 50, 101, 30, 200});
+    }
+
+    private void test(int sum, int[] numbers) {
+        Lunches lunches = new Lunches(numbers);
+        Assert.assertEquals(sum, lunches.sum());
+
+    }
+}
